0937c782407accab39e93cc3ffe7cda22b7635b4
mnink275 <nmingaripov@gmail.com>
2026-04-15 15:54:25 +0300

    feat: order system

--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -1,17 +1,22 @@
+import { useMemo, useState } from 'react';
 import './GamePage.css';
 import bg from './storage.jpg';
 import { GameStoreProvider } from './store';
+import { OrderPanel } from './OrderPanel';
+import { OrderGenerator, Order } from '../../game/OrderGenerator';
 
-interface GamePageProps {
-
-}
+interface GamePageProps {}
 
 export function GamePage(props: GamePageProps) {
+  const generator = useMemo(() => new OrderGenerator(4, 6), []);
+
+  const [order] = useState<Order>(() => generator.generate());
 
   return (
     <GameStoreProvider>
       <div className='GamePage-root'>
-        <img src={bg} alt='bg' className='GamePage-bg'/>
+        <img src={bg} alt='bg' className='GamePage-bg' />
+        <OrderPanel order={order} />
       </div>
     </GameStoreProvider>
   );
