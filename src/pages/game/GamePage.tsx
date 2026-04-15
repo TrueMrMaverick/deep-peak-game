2dfdfa28c7bb275c83e077c018735f9b8fd002ad
mnink275 <nmingaripov@gmail.com>
2026-04-15 16:01:55 +0300

    fix order logic

--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -1,11 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './GamePage.css';
 import bg from './storage.jpg';
 import { GameStoreProvider, useGameStoreContext } from './store';
 import { Shelves } from './components/Shelves';
+import { OrderPanel } from './OrderPanel';
+import { OrderGenerator, Order } from '../../game/OrderGenerator';
 
 function GameContent() {
   const store = useGameStoreContext();
+
+  const generator = useMemo(() => new OrderGenerator(4, 6), []);
+  const [order] = useState<Order>(() => generator.generate());
 
   useEffect(() => {
     store.startLoop();
@@ -16,6 +21,7 @@ function GameContent() {
     <div className='GamePage-root'>
       <img src={bg} alt='bg' className='GamePage-bg'/>
       <Shelves />
+      <OrderPanel order={order} />
     </div>
   );
 }
